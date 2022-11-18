5c16b8902c09b2765dc029113fe7ff5d08bd4e31
Thomas Woodland-Scott <thomaswoodland-scott@hotmail.com>
2022-11-18 10:51:04 +0000

    Add schema.sql, add db.py, edit dunder init.py's application factory function conecting db.py

---
+++ flaskr/db.py
@@ -0,0 +1,41 @@
+import sqlite3
+
+import click
+from flask import current_app, g # g is unique for each request, used to store data that is accessed multiple times during the request. 
+                                 # current_app points to the Flask app handling the request. Since we used an application factory, there is no application objext when writing the rest of the code.
+
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+@click.command('init-db')
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
+
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
